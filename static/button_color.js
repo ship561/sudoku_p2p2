8cb763fd2181071b65a503ec4af0377aac48ffc2
Vivi Wei <panwei@amazon.com>
2020-04-05 02:41:06 -0400

    add three color scheme

---
+++ static/button_color.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('#pur').click(function() {
+    $('body').css('background-color', '#FFCCCC');
+    $('h1').css('color', '#FF9999');
+    $('.game-container').css('background', '#996699');
+  });
+});
+
+$(document).ready(function() {
+  $('#bla').click(function() {
+    $('body').css('background-color', '#CCCCCC');
+    $('h1').css('color', '#CC3333');
+    $('.game-container').css('background', '#003366');
+  });
+});
+
+$(document).ready(function() {
+  $('#gld').click(function() {
+    $('body').css('background-color', '#F4F4F4');
+    $('h1').css('color', '#DCD0C0');
+    $('.game-container').css('background', '#C0B283');
+  });
+});
